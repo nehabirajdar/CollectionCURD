3ed0b688c9b58291c8cd17656ed0ce77ed039127
nehabirajdar <nsb9823@gmail.com>
2023-02-06 16:23:43 +0530

    Add project files.

---
+++ CollectionCURD/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace CollectionCURD
+{
+    public class Product
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        public int Price { get; set;}
+
+    }
+}
---
+++ CollectionCURD/ProductCRUD.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+
+namespace CollectionCURD
+{
+    public class ProductCRUD
+    {
+        private List<Product> products;
+        public ProductCRUD()
+        {
+            products = new List<Product>()
+            {
+                new Product{Id=101,Name="Connector",Price=799},
+                new Product{Id=102,Name="Pendrive",Price=999},
+            };
+        }
+
+        public void AddProduct(Product prod)
+        {
+            products.Add(prod);
+        }
+        public void UpdateProduct(Product prod)
+        {
+            for (int i = 0; i < products.Count; i++)
+            {
+                if (products[i].Id == prod.Id)
+                {
+                    products[i].Name = prod.Name;
+                    products[i].Price = prod.Price;
+                    break; // to break the loop
+                }
+            }
+        }
+        public void DeleteProduct(int id)
+        {
+            for (int i = 0; i < products.Count; i++)
+            {
+                if (products[i].Id == id)
+                {
+                    products.Remove(products[i]);
+                    break;
+                }
+            }
+        }
+        public List<Product> List()
+        {
+            return products;
+        }
+
+    }
+}
